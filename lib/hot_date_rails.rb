68800157f7f9648c2a6cd6a2941a47d3c6ff55a7
John O'Malley <jom@nycap.rr.com>
2014-09-23 00:03:18 -0400

    remove pry

--- lib/hot_date_rails.rb
+++ lib/hot_date_rails.rb
@@ -1,6 +1,6 @@
 require "hot_date_rails/engine"
 require 'coffee-rails'
-require 'pry-rails' unless Rails.env == "production"
+#require 'pry-rails' unless Rails.env == "production"
 require 'jquery-rails'
 
 module HotDateRails
