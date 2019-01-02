bce2d232ff86bfc63abe9e68cbaa871978b069c8
John OMalley <jom@nycap.rr.com>
2019-01-01 20:20:27 -0500

    Cherry-picked just activerecord and sprockets from rails. All specs green

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,15 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
+require 'rails/all'
+require 'mongoid'
 
 Bundler.require(*Rails.groups)
 require "hot_date_rails"
-require 'mongoid'
+
 
 module Dummy
   class Application < Rails::Application
