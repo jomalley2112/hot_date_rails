32f133ee533a36518d7bbc9eb5da5292a80a0cf0
JOM <jom@nycap.rr.com>
2019-03-22 15:21:31 -0400

    Resolves issue #12 (Get rid of ActiveRecord 5.2 boolean column deprecation warning)

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -7,11 +7,11 @@ require 'mongoid'
 Bundler.require(*Rails.groups)
 require "hot_date_rails"
 
-
 module Dummy
   class Application < Rails::Application
     config.time_zone = 'Eastern Time (US & Canada)'
 	  config.public_file_server.enabled = true
+	  config.active_record.sqlite3.represent_boolean_as_integer = true
   end
 end
 
